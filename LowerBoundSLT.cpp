1da8f426c5f86560bf28a41881a21023f46460a6
FadelSM <rasyahstore8888@gmail.com>
2026-02-08 13:32:57 +0700

    Add lower bound search functionality
    Implement lower bound search for a list of integers.

--- LowerBoundSLT.cpp
+++ LowerBoundSLT.cpp
@@ -5,8 +5,31 @@
 #include <algorithm>
 using namespace std;
 
-
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    int q;
+    cin >> q;
+
+    while(q--) {
+        int x;
+        cin >> x;
+
+        auto it = lower_bound(v.begin(), v.end(), x);
+        int idx = it - v.begin(); // 0-based index
+
+        if(it != v.end() && *it == x) {
+            cout << "Yes " << idx + 1 << endl;
+        } else {
+            cout << "No " << idx + 1 << endl;
+        }
+    }
+
     return 0;
 }
